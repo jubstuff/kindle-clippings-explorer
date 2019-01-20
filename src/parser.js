0a709a8317b68201ef38fca581dbcb2eccf24f62
Giustino Borzacchiello <giustinob@gmail.com>
2019-01-20 18:08:28 +0100

    Refactor date parsing in separate function

--- src/parser.js
+++ src/parser.js
@@ -1,35 +1,45 @@
-module.exports = clippingText => {
+function parseDateLine( dateTextLine ) {
 
-  const italianMonthNames = {
-    'gennaio': 1,
-    'febbraio': 2,
-    'marzo': 3,
-    'aprile': 4,
-    'maggio': 5,
-    'giugno': 6,
-    'luglio': 7,
-    'agosto': 8,
-    'settembre': 9,
-    'ottobre': 10,
-    'novembre': 11,
-    'dicembre': 12,
+  const ITALIAN_MONTH_NAMES = {
+    'gennaio': '01',
+    'febbraio': '02',
+    'marzo': '03',
+    'aprile': '04',
+    'maggio': '05',
+    'giugno': '06',
+    'luglio': '07',
+    'agosto': '08',
+    'settembre': '09',
+    'ottobre': '10',
+    'novembre': '11',
+    'dicembre': '12',
   };
 
+  const BASE_YEAR = 2000;
+
+  const dateText = dateTextLine.match( / il (.+)/ )[1];
+
+  const regex = /([A-Za-zì]+) (\d+) ([a-z]*) (\d+) (\d{2}:\d{2}:\d{2})/gm;
+  let [full, dayName, day, month, year, hour] = regex.exec( dateText );
+
+  day = parseInt( day );
+  month = ITALIAN_MONTH_NAMES[month];
+  year = parseInt( year ) + BASE_YEAR;
+
+  return new Date( `${year}-${month}-${day}T${hour}` )
+}
+
+module.exports = clippingText => {
   const lines = clippingText.trim().split( /\r?\n/ );
 
   const metaText = lines[1].trim();
   const metaTextLines = metaText.split( '|' );
   const positionText = metaTextLines[0].trim();
-  const dateText = metaTextLines[1].match( / il (.+)/ )[1];
-
-  const regex = /([A-Za-zì]+) (\d+) ([a-z]*) (\d+) (\d{2}:\d{2}:\d{2})/gm;
-  const [full, dayName, day, month, year, hour] = regex.exec( dateText );
 
   return {
     'title': lines[0].trim(),
     'meta': {
-      'date': new Date( parseInt(year) + 2000, italianMonthNames[month], day ),
-      'time': '',
+      'date': parseDateLine( metaTextLines[1].trim() ),
       'position': ''
     },
     'text': lines[3].trim()
--- test/parser-test.js
+++ test/parser-test.js
@@ -1,44 +1,44 @@
-const expect = require('chai').expect;
-const parse = require('../src/parser');
+const expect = require( 'chai' ).expect;
+const parse = require( '../src/parser' );
 
 
-describe('kindle clippings parser test', () => {
-  it('should pass this canary test', () => {
-    expect(true).to.be.true;
-  });
+describe( 'kindle clippings parser test', () => {
+  it( 'should pass this canary test', () => {
+    expect( true ).to.be.true;
+  } );
 
-  it('should split clipping in title, meta and text', () => {
+  it( 'should split clipping in title, meta and text', () => {
     const text = `Working Effectively with Legacy Code, First Edition (Michael Feathers)
 - Evidenziazione Pos. 2063-64  | Aggiunta il domenica 13 gennaio 19 17:03:49 Ora media di Greenwich
 
 Lag time is the amount of time that passes between a change that you make and the moment that you get real feedback about the change.
     `;
 
-    const clipping = parse(text);
+    const clipping = parse( text );
 
-    expect(clipping).to.have.property('title');
-    expect(clipping).to.have.property('meta');
-    expect(clipping).to.have.property('text');
-  });
+    expect( clipping ).to.have.property( 'title' );
+    expect( clipping ).to.have.property( 'meta' );
+    expect( clipping ).to.have.property( 'text' );
+  } );
 
-  it('should extract correct position and date for the clipping', () => {
+  it( 'should extract correct position and date for the clipping', () => {
     const clippingText = `Working Effectively with Legacy Code, First Edition (Michael Feathers)
 - Evidenziazione Pos. 2063-64  | Aggiunta il domenica 13 gennaio 19 17:03:49 Ora media di Greenwich
 
 Lag time is the amount of time that passes between a change that you make and the moment that you get real feedback about the change.`;
 
-    const clipping = parse(clippingText),
-    meta = clipping.meta;
-    expect(meta).to.be.an('object');
-    expect(meta).to.have.property('date');
-    expect(meta.date).to.be.a('date');
-    expect(meta.date.getDate()).to.equal(13);
-    expect(meta.date.getMonth()).to.equal(1);
-    expect(meta.date.getFullYear()).to.equal(2019);
-    expect(meta).to.have.property('position');
+    const clipping = parse( clippingText ),
+      meta = clipping.meta;
+    expect( meta ).to.be.an( 'object' );
+    expect( meta ).to.have.property( 'date' );
+    expect( meta.date ).to.be.a( 'date' );
+    expect( meta.date.getDate() ).to.equal( 13 );
+    expect( meta.date.getMonth() ).to.equal( 0 );
+    expect( meta.date.getFullYear() ).to.equal( 2019 );
+    expect( meta ).to.have.property( 'position' );
 
 
-  })
+  } )
 
 
-});
+} );
