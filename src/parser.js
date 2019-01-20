a91a70833a1d5a93099ceffd7b74fc8d89ea2cce
Giustino Borzacchiello <giustinob@gmail.com>
2019-01-20 17:56:25 +0100

    Parse meta: create Date object from Italian string

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,37 @@
-module.exports = function ( clippingText ) {
-  const lines = clippingText.trim().split(/\r?\n/);
+module.exports = clippingText => {
+
+  const italianMonthNames = {
+    'gennaio': 1,
+    'febbraio': 2,
+    'marzo': 3,
+    'aprile': 4,
+    'maggio': 5,
+    'giugno': 6,
+    'luglio': 7,
+    'agosto': 8,
+    'settembre': 9,
+    'ottobre': 10,
+    'novembre': 11,
+    'dicembre': 12,
+  };
+
+  const lines = clippingText.trim().split( /\r?\n/ );
+
+  const metaText = lines[1].trim();
+  const metaTextLines = metaText.split( '|' );
+  const positionText = metaTextLines[0].trim();
+  const dateText = metaTextLines[1].match( / il (.+)/ )[1];
+
+  const regex = /([A-Za-zì]+) (\d+) ([a-z]*) (\d+) (\d{2}:\d{2}:\d{2})/gm;
+  const [full, dayName, day, month, year, hour] = regex.exec( dateText );
 
   return {
     'title': lines[0].trim(),
-    'meta': lines[1].trim(),
+    'meta': {
+      'date': new Date( parseInt(year) + 2000, italianMonthNames[month], day ),
+      'time': '',
+      'position': ''
+    },
     'text': lines[3].trim()
   }
 };
--- test/parser-test.js
+++ test/parser-test.js
@@ -8,19 +8,37 @@ describe('kindle clippings parser test', () => {
   });
 
   it('should split clipping in title, meta and text', () => {
-    const text = `
-    Title
-    Meta
-    
-    Text
+    const text = `Working Effectively with Legacy Code, First Edition (Michael Feathers)
+- Evidenziazione Pos. 2063-64  | Aggiunta il domenica 13 gennaio 19 17:03:49 Ora media di Greenwich
+
+Lag time is the amount of time that passes between a change that you make and the moment that you get real feedback about the change.
     `;
 
     const clipping = parse(text);
 
-    expect(clipping).to.have.property('title').with.lengthOf(5);
-    expect(clipping).to.have.property('meta').with.lengthOf(4);
-    expect(clipping).to.have.property('text').with.lengthOf(4);
+    expect(clipping).to.have.property('title');
+    expect(clipping).to.have.property('meta');
+    expect(clipping).to.have.property('text');
   });
+
+  it('should extract correct position and date for the clipping', () => {
+    const clippingText = `Working Effectively with Legacy Code, First Edition (Michael Feathers)
+- Evidenziazione Pos. 2063-64  | Aggiunta il domenica 13 gennaio 19 17:03:49 Ora media di Greenwich
+
+Lag time is the amount of time that passes between a change that you make and the moment that you get real feedback about the change.`;
+
+    const clipping = parse(clippingText),
+    meta = clipping.meta;
+    expect(meta).to.be.an('object');
+    expect(meta).to.have.property('date');
+    expect(meta.date).to.be.a('date');
+    expect(meta.date.getDate()).to.equal(13);
+    expect(meta.date.getMonth()).to.equal(1);
+    expect(meta.date.getFullYear()).to.equal(2019);
+    expect(meta).to.have.property('position');
+
+
+  })
 
 
 });
